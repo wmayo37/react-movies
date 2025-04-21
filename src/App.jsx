7b2a134a5b2ac35de158b53fae021d98109e759d
Will Mayo <wm128@duke.edu>
2025-04-21 02:57:18 -0400

    added css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/favorites" element={<Favorites></Favorites>}></Route>
-      </Routes>
-    </main>
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/favorites" element={<Favorites></Favorites>}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
