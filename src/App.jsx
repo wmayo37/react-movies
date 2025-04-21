6a32df6e55418b57e77337099529476aa39e4658
Will Mayo <wm128@duke.edu>
2025-04-21 02:48:35 -0400

    added page routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
+import { Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/favorites" element={<Favorites></Favorites>}></Route>
+      </Routes>
+    </main>
   );
 }
 
